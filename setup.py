047c1da687fbe23bf60511d21055a64e049d3e02
Justin Patrin <papercrane@reversefold.com>
2015-02-21 15:28:03 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='reversefold.util',
+    version='1.0',
+    description='SSH, Multiproc, tail.py, and log.py',
+    author='Justin Patrin',
+    author_email='papercrane@reversefold.com',
+    url='https://github.com/reversefold/util',
+    packages=find_packages(),
+    scripts=['tail.py', 'log.py'],
+    license='MIT',
+    install_requires=[
+        'colorama',
+        'docopt',
+        'watchdog',
+    ],
+)
