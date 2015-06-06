c6dd92c3684d5bc7b300a9b479df1e282901157a
Justin Patrin <papercrane@reversefold.com>
2015-06-05 23:44:55 -0700

    Add daemonize.py which allows you to run an arbitrary command as a daemon while capturing its output in a watched file

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='reversefold.util',
-    version='1.5.0',
+    version='1.6.0',
     description='SSH, Multiproc, tail.py, log.py, stream.py',
     author='Justin Patrin',
     author_email='papercrane@reversefold.com',
     url='https://github.com/reversefold/util',
     packages=find_packages(),
-    scripts=['tail.py', 'log.py', 'stream.py'],
+    scripts=['tail.py', 'log.py', 'stream.py', 'daemonize.py'],
     license='MIT',
     install_requires=[
         'colorama',
         'docopt',
+        'python-daemon',
         'watchdog',
     ],
 )
