b6dfba98df750658876757847c67f3a66b09b9ba
Justin Patrin <papercrane@reversefold.com>
2016-04-26 22:13:15 -0700

    Add options to not use color in text output and to disable the capturing of output

--- reversefold/util/multiproc.py
+++ reversefold/util/multiproc.py
@@ -7,10 +7,17 @@ from colorama import Fore, Style
 
 DEFAULT_OUT_PREFIX = '%s[%s%sout%s%s]%s ' % (
     Style.BRIGHT, Style.NORMAL, Fore.GREEN, Fore.RESET, Style.BRIGHT, Style.NORMAL)
+DEFAULT_OUT_PREFIX_PLAIN = '[out] '
+
 DEFAULT_OUT_POSTFIX = ''
+DEFAULT_OUT_POSTFIX_PLAIN = ''
+
 DEFAULT_ERR_PREFIX = '%s[%s%serr%s%s]%s%s ' % (
     Style.BRIGHT, Style.NORMAL, Fore.RED, Fore.RESET, Style.BRIGHT, Style.NORMAL, Fore.RED)
+DEFAULT_ERR_PREFIX_PLAIN = '[err] '
+
 DEFAULT_ERR_POSTFIX = Fore.RESET
+DEFAULT_ERR_POSTFIX_PLAIN = ''
 
 
 class Pipe(object):
@@ -31,12 +38,13 @@ class Pipe(object):
 class LinePipe(object):
     # TODO: input and output_func are not symmetric, perhaps they should
     # use the same interface?
-    def __init__(self, prefix, input_stream, buf, output_func, postfix):
+    def __init__(self, prefix, input_stream, buf, output_func, postfix, capture_output):
         self.prefix = prefix
         self.input_stream = input_stream
         self.buf = buf
         self.output_func = output_func
         self.postfix = postfix
+        self.capture_output = capture_output
 
     def flow(self):
         # NOTE: 'for line in self.input' seems correct but will
@@ -44,16 +52,21 @@ class LinePipe(object):
         #  give you a line immediately every time one is available.
         # This is supposedly fixed in Python 3.2.
         for line in iter(self.input_stream.readline, b''):
-            self.buf.append(line)
+            if self.capture_output:
+                self.buf.append(line)
             self.output_func('%s%s%s\n' % (self.prefix, line.rstrip(), self.postfix))
 
 
+# TODO: Remove output capture entirely and allow the caller's output_func to handle it
+# TODO: Remove use_color and let the caller just specify non-colored versions
 def run_subproc(
     proc, prefix='', wait=True, output_func=None,
     out_prefix=None,
     out_postfix=None,
     err_prefix=None,
     err_postfix=None,
+    capture_output=True,
+    use_color=True
 ):
     """
     Runs a single subprocess, outputting then returning the stderr and stdout
@@ -61,13 +74,13 @@ def run_subproc(
     returns: stdout, stderr
     """
     if out_prefix is None:
-        out_prefix = DEFAULT_OUT_PREFIX
+        out_prefix = DEFAULT_OUT_PREFIX if use_color else DEFAULT_OUT_PREFIX_PLAIN
     if out_postfix is None:
-        out_postfix = DEFAULT_OUT_POSTFIX
+        out_postfix = DEFAULT_OUT_POSTFIX if use_color else DEFAULT_OUT_POSTFIX_PLAIN
     if err_prefix is None:
-        err_prefix = DEFAULT_ERR_PREFIX
+        err_prefix = DEFAULT_ERR_PREFIX if use_color else DEFAULT_ERR_PREFIX_PLAIN
     if err_postfix is None:
-        err_postfix = DEFAULT_ERR_POSTFIX
+        err_postfix = DEFAULT_ERR_POSTFIX if use_color else DEFAULT_ERR_POSTFIX_PLAIN
     if output_func is None:
         output_func = sys.stdout.write
     stdout = []
@@ -84,7 +97,7 @@ def run_subproc(
                 continue
             thread = threading.Thread(
                 name=prefix,
-                target=LinePipe(prefix, pipe, buf, output_func, postfix).flow)
+                target=LinePipe(prefix, pipe, buf, output_func, postfix, capture_output).flow)
             threads.append(thread)
             thread.start()
         if wait and proc.returncode is None:
