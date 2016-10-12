89b42685c67af050d070a637315ad7b160e8bfb7
Justin Patrin <papercrane@reversefold.com>
2016-10-11 19:02:34 -0700

    Move daemonize.py from scripts to a module so it can act as an entry_point

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='reversefold.util',
-    version='1.15.3',
+    version='1.15.4',
     description='SSH, Proc, Multiproc, tail.py, log.py, stream.py, daemonize.py, etc.',
     author='Justin Patrin',
     author_email='papercrane@reversefold.com',
     url='https://github.com/reversefold/util',
     packages=find_packages(),
-    scripts=['scripts/tail.py', 'scripts/log.py', 'scripts/stream.py', 'scripts/daemonize.py'],
+    scripts=['scripts/tail.py', 'scripts/log.py', 'scripts/stream.py'],
+    entry_points={
+        'console_scripts': [
+            'daemonize.py = reversefold.util.daemonize:main'
+        ],
+    },
     license='MIT',
     install_requires=[
         'colorama',
