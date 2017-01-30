21fa4a6aad2a20e6e4e2a267f229bfeda862c326
Justin Patrin <papercrane@reversefold.com>
2017-01-30 13:09:32 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reversefold.util',
-    version='1.15.4',
+    version='1.15.5',
     description='SSH, Proc, Multiproc, tail.py, log.py, stream.py, daemonize.py, etc.',
     author='Justin Patrin',
     author_email='papercrane@reversefold.com',
