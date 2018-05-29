a30e5c2fb36d25763fafa74f090f7eca51b89c3b
Justin Patrin <justin_patrin@mcafee.com>
2018-05-29 13:28:27 -0700

    Refactor followers so that the base follower follows a file-like object or an fd, then the higher level follower supports using a filename to open the files. This way we can support passing in a file-like object or fd directly.

--- reversefold/util/follow.py
+++ reversefold/util/follow.py
@@ -2,33 +2,10 @@ import os
 import time
 
 
-class LineFollower(object):
-    """
-    Implements a simple (naive) file streamer which will emit lines
-    until the finish property is set to False.
-
-    Ex:
-    with Follower('a') as f:
-        for line in f:
-            print line
-            if line == 'a':
-                f.finish = True
-    """
-    def __init__(self, filename, tail_only=False):
-        self.filename = filename
-        self.tail_only = tail_only
-        self.file = None
+class FileLineFollower(object):
+    def __init__(self, file):
+        self.file = file
         self.finish = False
-
-    def __enter__(self):
-        self.file = open(self.filename, 'r')
-        if self.tail_only:
-            # Go to the end of the file
-            self.file.seek(0, 2)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.file.close()
 
     def __iter__(self):
         buf = []
@@ -41,11 +18,62 @@ class LineFollower(object):
                 continue
             buf.append(new_line)
             if buf[-1][-1] == '\n':
+                # Don't include the trailing newline in the output
                 yield ''.join(buf)[:-1]
                 buf = []
 
 
-class Follower(object):
+class FilenameLineFollower(FileLineFollower):
+    """
+    Implements a simple (naive) file streamer which will emit lines
+    until the finish property is set to False.
+
+    Ex:
+    with Follower('a') as f:
+        for line in f:
+            print line
+            if line == 'a':
+                f.finish = True
+    """
+    def __init__(self, filename, tail_only=False):
+        # Explicitly setting file to None as we set it in __enter__
+        super(FilenameLineFollower, self).__init__(file=None)
+        self.filename = filename
+        self.tail_only = tail_only
+
+    def __enter__(self):
+        self.file = open(self.filename, 'r')
+        if self.tail_only:
+            # Go to the end of the file
+            self.file.seek(0, 2)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.file.close()
+        self.file = None
+
+
+LineFollower = FilenameLineFollower
+
+
+class FDFollower(object):
+    def __init__(self, fd, chunk_size):
+        self.fd = fd
+        self.chunk_size = chunk_size
+        self.finish = False
+
+    def __iter__(self):
+        while True:
+            data = os.read(self.fd, self.chunk_size)
+            if not data:
+                if self.finish:
+                    raise StopIteration()
+                time.sleep(0.1)
+                continue
+            yield data
+
+
+class FilenameFollower(FDFollower):
     """
     Implements a simple (naive) file streamer which will emit bytes
     until the finish property is set to False.
@@ -60,12 +88,10 @@ class Follower(object):
                 f.finish = True
     """
     def __init__(self, filename, tail_only=False, chunk_size=1024):
+        # Setting fd to None as we set it in __enter__
+        super(FilenameFollower, self).__init__(fd=None, chunk_size=chunk_size)
         self.filename = filename
         self.tail_only = tail_only
-        self.chunk_size = chunk_size
-
-        self.fd = None
-        self.finish = False
 
     def __enter__(self):
         self.fd = os.open(self.filename, os.O_RDONLY | os.O_NONBLOCK)
@@ -77,12 +103,5 @@ class Follower(object):
     def __exit__(self, exc_type, exc_value, traceback):
         os.close(self.fd)
 
-    def __iter__(self):
-        while True:
-            data = os.read(self.fd, self.chunk_size)
-            if not data:
-                if self.finish:
-                    raise StopIteration()
-                time.sleep(0.1)
-                continue
-            yield data
+
+Follower = FilenameFollower
